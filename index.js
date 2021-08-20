b5cbaeca1360c130797eac696c9db4e7a28a1c11
Riding Rails <ridingwithrails@gmail.com>
2021-08-20 03:49:31 +0000

    Adding helper

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2
-})
+const helpers = require('./helpers')
+const format = helpers.formatter.format
 
 const rate_of_return = (investment, payout) => {
   const rate = ((payout - investment) / investment) * 100
@@ -13,7 +10,7 @@ const annuity = (pv, t, r) => {
   let cf = pv * r
   const rate_annuity = 1 - (1 / Math.pow(1 + r, t))
   cf = cf / rate_annuity
-  return formatter.format(cf)
+  return format(cf)
 }
 
 console.log(annuity(1_000_000, 10, .10))
