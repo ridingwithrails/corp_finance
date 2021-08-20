909a9bcc6fd9cfa5376c237c49a4257711ed4aac
riding rails <ridingwithrails@gmail.com>
2021-08-20 23:54:41 +0000

    Added testing capability

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const helpers = require('./helpers')
 const format = helpers.formatter.format
 
-const rate_of_return = (investment, payout) => {
-  const rate = ((payout - investment) / investment) * 100
-  return rate
+module.exports = {
+  rate_of_return: (investment, payout) => {
+    const rate = ((payout - investment) / investment) * 100
+    return rate
+  },
+
+  annuity: (pv, t, r) => {
+    let cf = pv * r
+    const rate_annuity = 1 - (1 / Math.pow(1 + r, t))
+    cf = cf / rate_annuity
+    return format(cf)
+  },
+
+  future_value: (pv, r) => {
+    return (
+      pv * (1 + r)
+    )
+  },
+
+  present_value: (fv, r, t) => {
+    return (
+      fv / Math.pow((1 + r), t)
+    )
+  },
+
+  npv: (discount, cash_flows, time, price) => {
+    const value = cash_flows.reduce((total, cf) => {
+      return (total + cf / Math.pow((1 + discount), time))
+    })
+    return (-price + value)
+  }
 }
 
-const annuity = (pv, t, r) => {
-  let cf = pv * r
-  const rate_annuity = 1 - (1 / Math.pow(1 + r, t))
-  cf = cf / rate_annuity
-  return format(cf)
-}
 
-const future_value = (pv, r) => {
-  return (
-    pv * (1 + r)
-  )
-}
-
-const present_value = (fv, r, t) => {
-  return (
-    fv / Math.pow((1 + r), t)
-  )
-}
-
-const npv = (discount, cash_flows, time, price) => {
-  const value = cash_flows.reduce((total, cf) => {
-    return (total + cf / Math.pow((1 + discount), time))
-  })
-  return (-price + value)
-}
-
-console.log(annuity(1_000_000, 10, .10))
